58fe262bbb448a9b9bfac4d8a4febbb90203fa44
Qrofeus <ninad9021@gmail.com>
2023-05-08 11:22:35 +0530

    Plug-Board (presets modification, testing)

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from plugBoard import PlugBoard
+from data.test_paragraphs import paragraphs
+import random
+
+
+# def test_plug_board(paragraph: str, plugboard: PlugBoard):
+#     print(plugboard.plugs)
+#
+#     print(paragraph)
+#     cipher_1 = cipher_paragraph(paragraph, plugboard)
+#     print(cipher_1)
+#     cipher_2 = cipher_paragraph(cipher_1, plugboard)
+#     print(cipher_2)
+#
+#
+# def cipher_paragraph(message: str, plugboard: PlugBoard) -> str:
+#     output = ""
+#     for letter in message:
+#         if letter.isalpha():
+#             c_letter = plugboard.cipher(letter)
+#             letter = c_letter.upper() if letter.isupper() else c_letter
+#         output += letter
+#     return output
+
+
 def main():
-    pass
+    plug_board = PlugBoard()
+    paragraph = random.choice(paragraphs)
+
+    # test_plug_board(paragraph, plug_board)
 
 
 if __name__ == '__main__':
--- plugBoard.py
+++ plugBoard.py
@@ -3,18 +3,28 @@ from string import ascii_lowercase
 import random
 
 
-class InvalidPreset(Exception):
+class InvalidPresetCode(Exception):
     pass
 
 
+def check_code(code: str) -> bool:
+    """Returns True if code matches required format, else False
+    Format -> Single character from lowercase english alphabet (re: "[a-z]")"""
+    if len(code) == 1 and code in ascii_lowercase:
+        return True
+    return False
+
+
 class PlugBoard:
-    def __init__(self, preset_char: str = None) -> None:
+    def __init__(self, code: str = None) -> None:
         """Creates a PlugBoard object with preset positions selected according to the preset character provided
          or chosen randomly from the data"""
-        if not preset_char:
+        if not code:
             preset = random.randint(0, 25)
         else:
-            preset = ascii_lowercase.index(preset_char)
+            if not check_code(code):
+                raise InvalidPresetCode
+            preset = ascii_lowercase.index(code)
         self.preset_index = preset
         self.plugs: dict = presets[self.preset_index]
 
@@ -22,12 +32,12 @@ class PlugBoard:
         """Returns the preset for current position as an alphabet"""
         return ascii_lowercase[self.preset_index]
 
-    def set_preset(self, preset_char: str) -> None:
+    def set_preset(self, code: str) -> None:
         """According to the provided alphabet, selects the preset from the data
         :raises InvalidPreset Exception if length of preset_char is not equal to 1"""
-        if len(preset_char) != 1:
-            raise InvalidPreset
-        self.preset_index = ascii_lowercase.index(preset_char)
+        if not check_code(code):
+            raise InvalidPresetCode
+        self.preset_index = ascii_lowercase.index(code)
         self.plugs: dict = presets[self.preset_index]
 
     def cipher(self, letter: str) -> str:
