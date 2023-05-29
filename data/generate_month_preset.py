d936988b862074573840161156d683905ac03c16
Qrofeus <ninad9021@gmail.com>
2023-05-29 18:44:34 +0530

    Revert "Update generate_month_preset.py"
    This reverts commit 6e75ccdd7707435844729e399946b0b1b67dbe46.

--- data/generate_month_preset.py
+++ data/generate_month_preset.py
@@ -40,7 +40,7 @@ def generate_month(days: int) -> list:
 def main():
     today = datetime.date.today()
     year, month = today.year, (today.month + 1)
-    f_path = f"{year}/{month:02}.dat"
+    f_path = f"./{year}/{month:02}.dat"
     print(f"Attempting month-preset creation for {month=:02}, {year=}")
 
     if not os.path.exists(f_path):
